2ea77e0f5fe1c55ea504ffa7ee9ba43961a67c06
leticialima-dev <leticiabispo.limargmail.com>
2022-03-05 09:35:10 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputTextComponent } from './input-text/input-text.component';
+import { InputNumberComponent } from './input-number/input-number.component';
+import { InputEmailComponent } from './input-email/input-email.component';
+import { InputButtonComponent } from './input-button/input-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputTextComponent,
+    InputNumberComponent,
+    InputEmailComponent,
+    InputButtonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/input-button/input-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button',
+  templateUrl: './input-button.component.html',
+  styleUrls: ['./input-button.component.css']
+})
+export class InputButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
